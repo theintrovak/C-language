e61c68fc1a5aaafcb0a3542015f6004ef3ce292b
Anurag Chaudhary <anuragchaudhary903@gmail.com>
2026-09-24 00:33:55 +0530

    thierd question

---
+++ 01-basics/circleArea.c
@@ -0,0 +1,11 @@
+//write a program to find area of a circle
+#include<stdio.h>
+int main(){
+
+float pi = 3.14 , a , r ;
+printf("enter the value of radious  ");
+scanf("%f",&r);
+a = 2*pi*r;
+printf("the area of circle is = %f",a);
+
+}
