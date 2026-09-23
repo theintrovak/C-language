630d486b8714c71ef40973ce0568244c9e864af1
Anurag Chaudhary <anuragchaudhary903@gmail.com>
2026-09-23 22:54:12 +0530

    first push

---
+++ 01-basics/equationSolve.c
@@ -0,0 +1,9 @@
+//write a program to solve this equation S = (n-2)(n-1)/6
+#include<stdio.h>
+int main(){
+    int n, s ;
+    printf("enter the value of n = ");
+    scanf("%d",&n);
+    s = (n-2)*(n-1)/6;
+    printf("the value of solution is = %d",s);
+}
---
+++ 01-basics/program1.c
@@ -0,0 +1,11 @@
+// write  a program for the sum of two numbers 
+#include<stdio.h>
+int main(){
+ int a , b , c ;
+ printf("enter the value of a ");
+ scanf("%d",&a);
+ printf("enter the value of b ");
+ scanf("%d",&b);
+ c = a+b;
+ printf("the sum is = %d",c);
+}
